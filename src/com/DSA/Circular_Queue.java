f302f588d518345bb5f8b184390878a8ba2624cb
Rachit <rachit2912chaudhary@gmail.com>
2022-07-29 10:30:43 +0530

    Initial commit

---
+++ src/com/DSA/Circular_Queue.java
@@ -0,0 +1,64 @@
+package com.DSA;
+
+class circularQueue{
+    int size;
+    int r;
+    int f;
+    int [] arr;
+
+    circularQueue (int size){
+        this.r = this.f = 0;
+        this.size = size;
+        this.arr = new int[this.size];
+    }
+
+    public Boolean isEmpty (){
+        return this.f == this.r;
+    }
+
+    public Boolean isFull (){
+        return (this.f + 1)%(this.size) == this.r;
+    }
+
+    public void enQueue (int val){
+        if (this.isFull()){
+            System.out.println("Queue Overflow");
+        }
+
+        else {
+            this.arr[this.f] = val;
+            this.f = (this.f + 1)%this.size;
+        }
+    }
+
+    public int deQueue (){
+        int item = -1;
+        if (isEmpty()){
+            System.out.println("Queue Underflow");
+        }
+
+        else {
+            this.r = (this.r + 1) % this.size;
+            item = this.arr[this.r];
+        }
+        return item;
+    }
+}
+
+public class Circular_Queue {
+    public static void main(String[] args) {
+        circularQueue obj = new circularQueue(4);
+
+        obj.enQueue(1);
+        obj.enQueue(1);
+        obj.enQueue(1);
+
+        obj.deQueue();
+        obj.deQueue();
+        obj.deQueue();
+
+        obj.enQueue(1);
+        obj.enQueue(1);
+        obj.enQueue(1);
+    }
+}
