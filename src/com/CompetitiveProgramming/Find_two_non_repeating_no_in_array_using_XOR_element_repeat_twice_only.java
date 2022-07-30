9fae9cc6cb0a66054fe66da8984f31a926a1bfd9
Rachit <rachit2912chaudhary@gmail.com>
2022-07-30 17:04:48 +0530

    Initial commit

--- src/com/CompetitiveProgramming/Find_two_non_repeating_no_in_array_using_XOR_element_repeat_twice_only.java
+++ src/com/CompetitiveProgramming/Find_two_non_repeating_no_in_array_using_XOR_element_repeat_twice_only.java
@@ -5,27 +5,28 @@ import java.util.ArrayList;
 public class Find_two_non_repeating_no_in_array_using_XOR_element_repeat_twice_only {
     public static void main(String[] args) {
         int unique = 0;
-        int [] arr = {1, 2, 2, 1, 3, 4};
+        int [] arr = {1, 2, 2, 1, 3, 3, 4};
 
         for (int element: arr){
             unique ^= element;
         }
 
+        unique  = unique & -unique;
         int first_no = 0;
         int Second_no = 0;
         for (int element: arr){
-            if ((element & 1) == 1){
-                first_no = unique ^ element;
+            if ((element & unique) > 0){
+                first_no = first_no ^ element;
             }
 
             else{
-                Second_no = unique ^ element;
+                Second_no = Second_no ^ element;
             }
 
         }
 
-        if (first_no == 0 || Second_no == 0){
-            System.out.println("There is only one unique element, "+ unique);
+        if (Second_no == 0){
+            System.out.println("There is only one unique element, "+ first_no);
         }
 
         else if (unique == 0){
