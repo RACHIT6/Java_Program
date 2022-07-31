8eff0c428170816028efa3442c59c170ad873bd3
Rachit <rachit2912chaudhary@gmail.com>
2022-07-31 12:51:00 +0530

    Initial commit

---
+++ src/com/DSA/Stack_Array.java
@@ -0,0 +1,63 @@
+package com.DSA;
+
+class Stack{
+    int size;
+    int top = -1;
+    int [] arr;
+
+    Stack (int size){
+        this.size = size;
+        this.arr = new int[this.size];
+    }
+    public Boolean isEmpty (){
+        return this.top == -1;
+    }
+
+    public Boolean isFull (){
+        return this.size - 1 == this.top;
+    }
+
+    public void push (int val){
+        if (this.isFull()){
+            System.out.println("Stack is Full");
+            return;
+        }
+
+        this.top++;
+        this.arr[this.top] = val;
+    }
+
+    public int pop (){
+        if (this.isEmpty()){
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+
+        int PopValue = this.arr[this.top];
+        this.top--;
+        return PopValue;
+    }
+
+    public int peek (){
+        return this.arr[this.top];
+    }
+
+    public void Print (){
+        for (int i = 0; i <= this.top; i++) {
+            System.out.print(this.arr[i] +" ");
+        }
+    }
+}
+
+public class Stack_Array {
+    public static void main(String[] args) {
+        Stack obj = new Stack(5);
+        obj.push(12);
+        obj.push(12);
+        obj.push(12);
+        obj.push(12);
+        obj.push(12);
+        obj.pop();
+        obj.Print();
+    }
+}
