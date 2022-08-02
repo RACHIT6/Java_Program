1098489047a689bb4c2a8580084b4da266e5c6ea
Rachit <rachit2912chaudhary@gmail.com>
2022-08-02 13:12:26 +0530

    Initial commit

--- src/com/DSA/Level_Order_Traversal.java
+++ src/com/DSA/Level_Order_Traversal.java
@@ -1,16 +1,5 @@
 package com.DSA;
 
-
 public class Level_Order_Traversal {
-    public static void levelOrder (Tree root){
-        if (root == null){
-            return;
-        }
 
-        Queue list = new LinkedList<>();
-    }
-
-    public static void main(String[] args) {
-
-    }
 }
