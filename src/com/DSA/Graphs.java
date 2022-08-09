7fa95112d047a75ea93a1d55e269b49301e7e6db
Rachit <rachit2912chaudhary@gmail.com>
2022-08-09 15:33:43 +0530

    Initial commit

--- src/com/DSA/Graphs.java
+++ src/com/DSA/Graphs.java
@@ -1,8 +1,35 @@
 package com.DSA;
-import java.util.Queue;
+import java.util.ArrayList;
 
 public class Graphs {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(); // Implemented as Queue;
+        int [] visited = {0, 0, 0, 0, 0, 0, 0};
+        int i = 5;
 
+//        Adjacency Matrix.
+        int [][] connections ={
+                {0, 1, 1, 1, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0},
+                {1, 1, 0, 1, 1, 0, 0},
+                {1, 0, 1, 0, 1, 0, 0},
+                {0, 0, 1, 1, 0, 1, 1},
+                {0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0},
+        };
+
+        System.out.print(i);
+        visited[i] = 1;
+        list.add(i);
+        while (!list.isEmpty()){
+            int node = list.remove(0);
+            for (int j = 0; j < 7; j++) {
+                if (connections[node][j] == 1 && visited[j] == 0){
+                    System.out.print(" "+ j);
+                    visited[j] = 1;
+                    list.add(j);
+                }
+            }
+        }
     }
 }
